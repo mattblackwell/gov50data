f4b03440e3f8d99f8f278fd813c38b820f55d0d4
Matthew Blackwell <mblackwell@gov.harvard.edu>
2023-10-30 21:06:21 -0400

    new class years

--- data-raw/make-class_years.R
+++ data-raw/make-class_years.R
@@ -1,8 +1,7 @@
 library(tidyverse)
 
 
-class_years <- read_csv("classyears_raw.csv", show_col_types = FALSE) |>
-  rename(year = Level)
+class_years <- read_csv("classyears_raw.csv", show_col_types = FALSE)
 
 save(class_years, file = "../data/class_years.rda")
 write_csv(class_years, file = "class_years.csv")
